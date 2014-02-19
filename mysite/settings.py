2d719d80b7370e30d4b2251b13f577ff481110e8
parkjuram <parkjuram@gmail.com>
2014-02-19 18:34:52 +0900

    setting change test

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,10 +56,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'beauty',
+        'USER': 'arsdale',
+        'PASSWORD': 'parkjuram90',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
